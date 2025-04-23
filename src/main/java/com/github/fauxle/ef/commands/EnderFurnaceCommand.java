f9a126806d236878da6b3a0333a6bca74c6f2725
Fauxle <151577161+Fauxle@users.noreply.github.com>
2025-04-23 13:31:17 -0400

    Open inventory

--- src/main/java/com/github/fauxle/ef/commands/EnderFurnaceCommand.java
+++ src/main/java/com/github/fauxle/ef/commands/EnderFurnaceCommand.java
@@ -1,9 +1,14 @@
 package com.github.fauxle.ef.commands;
 
 import com.github.fauxle.ef.EnderFurnacePlugin;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 
 public class EnderFurnaceCommand implements CommandExecutor {
 
@@ -14,8 +19,14 @@ public class EnderFurnaceCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
-        return false;
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof HumanEntity p)){
+            sender.sendMessage(ChatColor.RED + "Error: Only human entities (players) can execute this command");
+            return true;
+        }
+        Inventory inventory = plugin.getServer().createInventory(null, InventoryType.CHEST);
+        p.openInventory(inventory);
+        return true;
     }
 
 }

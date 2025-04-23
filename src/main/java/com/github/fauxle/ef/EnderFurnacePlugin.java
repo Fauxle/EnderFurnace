eed9c995d919ce02c5e8a36f88b39c646a75d50a
Fauxle <151577161+Fauxle@users.noreply.github.com>
2025-04-23 11:38:36 -0400

    Initial commit

---
+++ src/main/java/com/github/fauxle/ef/EnderFurnacePlugin.java
@@ -0,0 +1,12 @@
+package com.github.fauxle.ef;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EnderFurnacePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+}
